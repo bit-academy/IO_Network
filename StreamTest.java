38287d0a2cc4800907a30fe5cc9ff52ca86b6f3b
mindwing <mindwing@gmail.com>
2015-07-27 10:39:50 +0900

    .

--- StreamTest.java
+++ StreamTest.java
@@ -1,28 +1,23 @@
 import java.io.*;
 
-public class StreamTest
-{
-    public static void main(String[] args) throws IOException
-    {
-        System.out.println(System.in);
-        System.out.println(System.out);
+public class StreamTest {
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.in);
+		System.out.println(System.out);
 
-        for (int input; (input=System.in.read())!=-1; )
-        {
-            if (input=='x')
-            {
-                System.exit(0);
-            }
+		for (int input; (input = System.in.read()) != -1;) {
+			if (input == 'x') {
+				System.exit(0);
+			}
 
-            System.out.print("ÀÔ·Â : ");
+			System.out.print("ìž…ë ¥: ");
 
-            if (input!=10 && input!=13)
-            {
-                System.out.write(input);
-                System.out.print(", ");
-            }
+			if (input != 10 && input != 13) {
+				System.out.write(input);
+				System.out.print(", ");
+			}
 
-            System.out.println(input);
-        }
-    }
+			System.out.println(input);
+		}
+	}
 }
