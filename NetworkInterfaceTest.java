17e0015f2ca23521f4d7360fa92c6a529a1593f2
mindwing <mindwing@gmail.com>
2015-07-31 01:48:22 +0900

    .

--- NetworkInterfaceTest.java
+++ NetworkInterfaceTest.java
@@ -1,14 +1,12 @@
 import java.net.*;
 import java.util.*;
 
-public class NetworkInterfaceTest
-{
-    public static void main(String[] args) throws Exception
-    {
-        Enumeration enum=NetworkInterface.getNetworkInterfaces();
-        while(enum.hasMoreElements())
-        {
-            System.out.println(enum.nextElement());
+public class NetworkInterfaceTest {
+    public static void main(String[] args) throws Exception {
+        Enumeration enums = NetworkInterface.getNetworkInterfaces();
+
+        while (enums.hasMoreElements()) {
+            System.out.println(enums.nextElement());
         }
     }
 }
