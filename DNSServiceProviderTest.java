16a000db649fb3613e628f4a24648b7e89f132df
mindwing <mindwing@gmail.com>
2015-08-03 06:14:58 +0900

    .

--- DNSServiceProviderTest.java
+++ DNSServiceProviderTest.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
-import javax.naming.*;
-import javax.naming.directory.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 
 public class DNSServiceProviderTest {
     public static void main(String[] args) throws Exception {
@@ -11,19 +16,19 @@ public class DNSServiceProviderTest {
         env.put(Context.PROVIDER_URL, "dns://202.30.143.11");
         DirContext ctx = new InitialDirContext(env);
 
-        System.out.println(" === ������ ������ ��������.");
+        System.out.println(" === 도메인 네임을 넣으세요.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (String str; (str = br.readLine()) != null;) {
             Attributes attrs = ctx.getAttributes(str);
             if (attrs == null) {
-                System.out.println("��ϵ� ������ �����ϴ�.");
+                System.out.println("등록된 정보가 없습니다.");
                 continue;
             }
 
             for (NamingEnumeration enums = attrs.getAll(); enums.hasMore();) {
                 System.out.println(enums.next());
             }
-            System.out.println("\n === ������ ������ ��������.");
+            System.out.println("\n === 도메인 네임을 넣으세요.");
         }
     }
 }
